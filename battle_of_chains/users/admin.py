43424cc707a94fb5cad51aa5aa56048a74c907bc
Alexey Mihailov <alexey.mihailov@ubex.com>
2021-11-22 20:36:11 +0300

    fix: UserAdmin

--- battle_of_chains/users/admin.py
+++ battle_of_chains/users/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        (_("Personal info"), {"fields": ("email",)}),
         (
             _("Permissions"),
             {
