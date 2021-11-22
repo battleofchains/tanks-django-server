fbd07f43fcd1f3433a698d9ff4d29a78a2c6b3ac
Alexey Mihailov <alexey.mihailov@ubex.com>
2021-11-22 18:05:01 +0300

    chores: allow hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="gKGb0Y8srw6foLaObElgjrNer1wuhIhviKUNGkONdhgua3dvQJ8rjZVlq5SF5Vxk",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
