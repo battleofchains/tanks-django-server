22547ec512810e0447e72d059b600b3068f3e15d
Alexey Mihailov <alexey.mihailov@ubex.com>
2021-12-22 20:32:16 +0300

    feat: add NFT model

--- battle_of_chains/blockchain/models.py
+++ battle_of_chains/blockchain/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+from battle_of_chains.battle.models import Tank
+
 
 class Wallet(models.Model):
-    address = models.CharField(max_length=42, verbose_name='Wallet address', primary_key=True)
+    address = models.CharField(max_length=50, verbose_name='Wallet address', primary_key=True)
     date_add = models.DateTimeField(auto_now_add=True, verbose_name='Creation date')
     last_modified = models.DateTimeField(auto_now=True, verbose_name='Last update')
 
     def __str__(self):
         return self.address
+
+
+class NFT(models.Model):
+    address = models.CharField(max_length=50, primary_key=True)
+    tank = models.OneToOneField(Tank, on_delete=models.SET_NULL, null=True)
+    for_sale = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=15, decimal_places=6)
