b063be1f20549d9f8e974a375a957af7ec0c3d24
Alexey Mihailov <alexey.mihailov@ubex.com>
2021-12-27 22:10:49 +0300

    feat: add contract definitions, fix deploying to BSC

--- battle_of_chains/blockchain/utils.py
+++ battle_of_chains/blockchain/utils.py
@@ -4,6 +4,7 @@ import os
 
 from django.conf import settings
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 from battle_of_chains.blockchain.models import Contract
 
@@ -13,7 +14,9 @@ logger = logging.getLogger(__name__)
 def send_transaction(w3, txn, owner_address, owner_secret):
     try:
         gas = txn.estimateGas()
-        txn = txn.buildTransaction({'nonce': w3.eth.getTransactionCount(owner_address), 'gas': gas})
+        txn = txn.buildTransaction(
+            {'nonce': w3.eth.getTransactionCount(owner_address), 'gas': gas, "gasPrice": w3.eth.gas_price}
+        )
         signed_txn = w3.eth.account.signTransaction(txn, owner_secret)
         transaction = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
         tx_receipt = w3.eth.waitForTransactionReceipt(transaction)
@@ -30,14 +33,15 @@ def send_transaction(w3, txn, owner_address, owner_secret):
 
 def deploy_smart_contract(contract: Contract):
     w3 = Web3(Web3.HTTPProvider(contract.network.rpc_url, request_kwargs={'timeout': 600}))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     owner = settings.CONTRACTS_OWNER
     w3.eth.defaultAccount = owner['address']
     with open(os.path.join(settings.ROOT_DIR, 'contracts', 'nft.json')) as f:  # TODO: change later
         contract_definitions = json.load(f)
     if not contract.address:
         logger.debug(f'Deploy...')
-        contract = w3.eth.contract(abi=contract_definitions['abi'], bytecode=contract_definitions['bytecode'])
-        txn = contract.constructor()
+        smart_contract = w3.eth.contract(abi=contract_definitions['abi'], bytecode=contract_definitions['bytecode'])
+        txn = smart_contract.constructor()
         tx_receipt = send_transaction(w3, txn, owner['address'], owner['secret'])
         contract_address = tx_receipt['contractAddress']
         contract.address = contract_address
