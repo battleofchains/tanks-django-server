878abbcedf17fe501d203072a46ac4f6a1d83015
Alexey Mihailov <alexey.mihailov@ubex.com>
2022-01-26 21:09:43 +0300

    feat(marketplace): pagination done

--- battle_of_chains/market/reflexes/market_reflex.py
+++ battle_of_chains/market/reflexes/market_reflex.py
@@ -12,6 +12,7 @@ class MarketReflex(Reflex):
         filter_name = self.element.dataset['filter']
         filter_value = self.element.attributes['value']
         filters = self.request.session.get('filters', defaultdict(set))
+        order_by = self.request.session.get('order_by', '-date_add')
         if self.element.attributes['type'] == 'checkbox':
             if self.element.attributes['checked']:
                 filters[filter_name].add(filter_value)
@@ -26,8 +27,9 @@ class MarketReflex(Reflex):
                 del filters[filter_name]
         self.request.session['filters'] = filters
         queryset = context['paginator'].object_list
-        queryset = queryset.filter(**filters)
+        queryset = queryset.filter(**filters).order_by(order_by)
         paginator = Paginator(queryset, BattleSettings.get_solo().tanks_per_page)
+        context['order_by'] = order_by
         context['tanks'] = paginator.page(1)
         context['paginator'] = paginator
 
@@ -47,13 +49,24 @@ class MarketReflex(Reflex):
         if filters:
             queryset = queryset.filter(**filters)
         value = self.element.attributes['value']
-        match value:
-            case 'max_price':
-                queryset = queryset.order_by('-price')
-            case 'min_price':
-                queryset = queryset.order_by('price')
-            case _:
-                queryset = queryset.order_by('-date_mod')
+        queryset = queryset.order_by(value)
+        self.request.session['order_by'] = value
+        context['order_by'] = value
         paginator = Paginator(queryset, BattleSettings.get_solo().tanks_per_page)
         context['tanks'] = paginator.page(1)
         context['paginator'] = paginator
+
+    def page(self):
+        context = self.get_context_data()
+        filters = self.request.session.get('filters', None)
+        order_by = self.request.session.get('order_by', '-date_add')
+        page_number = self.element.dataset['page']
+        context['order_by'] = order_by
+        paginator = context['paginator']
+        queryset = paginator.object_list
+        if filters:
+            queryset = queryset.filter(**filters)
+        queryset = queryset.order_by(order_by)
+        paginator = Paginator(queryset, BattleSettings.get_solo().tanks_per_page)
+        context['paginator'] = paginator
+        context['tanks'] = paginator.page(int(page_number))
--- battle_of_chains/market/views.py
+++ battle_of_chains/market/views.py
@@ -11,9 +11,11 @@ class MarketPlaceView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MarketPlaceView, self).get_context_data(**kwargs)
         battle_settings = BattleSettings.get_solo()
+        context['orders'] = {'-date_mod': 'Newest', '-price': 'Max Price', 'price': 'Min Price'}
+        context['order_by'] = '-date_mod'
         tanks = Tank.objects.filter(
             Q(for_sale=True, basic_free_tank=False) | Q(basic_free_tank=True, offer__is_active=True)
-        ).order_by('-date_mod')
+        ).order_by(context['order_by'])
         context['type_filter'] = TankType.objects.values_list('id', 'name')
         paginator = Paginator(tanks, battle_settings.tanks_per_page)
         context['paginator'] = paginator
