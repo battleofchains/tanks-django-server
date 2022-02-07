6bcc4c9904507d3a585b0355872df7da9b46ea57
Alexey Mihailov <alexey.mihailov@ubex.com>
2022-02-07 11:53:20 +0300

    feat(marketplace): add primary/secondary sorting, add price icons

--- battle_of_chains/market/views.py
+++ battle_of_chains/market/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.db.models import Max, Q
+from django.db.models import Max, Q, F
 from django.views.generic import DetailView, TemplateView
 
 from battle_of_chains.battle.models import BattleSettings, Tank, TankType
@@ -11,11 +11,12 @@ class MarketPlaceView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MarketPlaceView, self).get_context_data(**kwargs)
         battle_settings = BattleSettings.get_solo()
-        context['orders'] = {'-date_mod': 'Newest', '-price': 'Max Price', 'price': 'Min Price'}
+        context['orders'] = {'-date_mod': 'Newest', '-price': 'Max Price', 'price': 'Min Price',
+                             'primary': 'Primary', '-primary': 'Secondary'}
         context['order_by'] = '-date_mod'
         tanks = Tank.objects.filter(
             Q(for_sale=True, basic_free_tank=False) | Q(basic_free_tank=True, offer__is_active=True)
-        ).order_by(context['order_by'])
+        ).annotate(primary=F('offer__is_active')).order_by(context['order_by'])
         context['type_filter'] = TankType.objects.values_list('id', 'name')
         paginator = Paginator(tanks, battle_settings.tanks_per_page)
         context['paginator'] = paginator
